290f7a37ac8d221c2134039e16a2b9aec6c5fb03
Nebahat Gueney <elsurneb@gmail.com>
2021-04-12 16:19:35 +0200

    notask prepare photographer app for the main container

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderPhotographer = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId)
+  );
+  unregister();
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.unmountRestaurant = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
